a7a6800a95d75d6c099ec3e65ee3bfd77d30afb3
BzykuuDM <bzyczek.dm@gmail.com>
2022-04-03 00:39:00 +0200

    9/24

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook:BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
@@ -12,6 +13,43 @@ namespace GradeBook.GradeBooks
         }
         public override char GetLetterGrade(double averageGrade)
         {
+            var top20 = 0;
+            
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            //top20 = Student.AverageGrade * 0.2;
+            var indexes = Students.Select(t => t.AverageGrade).ToList();
+            top20 = (int)Math.Ceiling(Students.Count * 0.2);
+
+            if (indexes[top20] < averageGrade)
+            {
+                return 'A';
+            }
+
+            if(indexes[top20*2] < averageGrade)
+            {
+                return 'B';
+            }
+
+            if (indexes[top20 * 3] < averageGrade)
+            {
+                return 'C';
+            }
+
+            if (indexes[top20 * 4] < averageGrade)
+            {
+                return 'D';
+            }
+
+            if (indexes[top20 * 5] < averageGrade)
+            {
+                return 'F';
+            }
+
+
             return base.GetLetterGrade(averageGrade);
         }
     }
